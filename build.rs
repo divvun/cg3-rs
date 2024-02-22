230d8f58daa1a7a34253432130ac928bb15ab97c
Brendan Molloy <brendan@necessary.nu>
2024-02-22 13:29:02 +0100

    Hm

--- build.rs
+++ build.rs
@@ -1,25 +1,26 @@
 use std::path::PathBuf;
 
 fn main() {
-    #[cfg(windows)]
-    let sysroot = PathBuf::from(std::env::var("SYSROOT").unwrap());
+    // #[cfg(windows)]
+    // let sysroot = PathBuf::from(std::env::var("SYSROOT").unwrap());
 
     #[cfg(windows)]
     let dst = cmake::Config::new("cg3")
         .define("WIN32", "ON")
-        .define("CMAKE_CXX_FLAGS", "/Dcg3_EXPORTS /DWIN32 /D_WIN32 /D_WINDOWS /W3 /GR /EHsc")
-        .define("CMAKE_TOOLCHAIN_FILE", r"D:\vcpkg\scripts\buildsystems\vcpkg.cmake")
-        .define("SQLITE3_INCLUDE_DIRS", r"D:\vcpkg\installed\x64-windows\include")
-        .define("SQLITE3_LIBRARIES", r"D:\vcpkg\installed\x64-windows\lib\sqlite3.lib")
+        .define("MSVC", "ON")
+        // .define("CMAKE_CXX_FLAGS", "/Dcg3_EXPORTS /DWIN32 /D_WIN32 /D_WINDOWS /W3 /GR /EHsc")
+        // .define("CMAKE_TOOLCHAIN_FILE", r"D:\vcpkg\scripts\buildsystems\vcpkg.cmake")
+        // .define("SQLITE3_INCLUDE_DIRS", r"D:\vcpkg\installed\x64-windows\include")
+        // .define("SQLITE3_LIBRARIES", r"D:\vcpkg\installed\x64-windows\lib\sqlite3.lib")
         // .define("ICU_LIBRARY_DIRS", sysroot.join("lib64"))
-        .define("BUILD_SHARED_LIBS", "off")
-        .define("Boost_INCLUDE_DIR", sysroot.join("include"))
+        .define("BUILD_SHARED_LIBS", "OFF")
+        // .define("Boost_INCLUDE_DIR", sysroot.join("include"))
         // .define("ICU_INCLUDE_DIR", sysroot.join("include"))
         .build();
 
     #[cfg(unix)]
     let dst = cmake::Config::new("cg3")
-        .define("BUILD_SHARED_LIBS", "off")
+        .define("BUILD_SHARED_LIBS", "OFF")
         .build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
@@ -39,8 +40,13 @@ fn main() {
     println!("cargo:rustc-link-lib=static=cg3");
     println!("cargo:rustc-link-lib=static=icuuc");
     println!("cargo:rustc-link-lib=static=icuio");
-    println!("cargo:rustc-link-lib=static=icudata");
-    println!("cargo:rustc-link-lib=static=icui18n");
+    if cfg!(windows) {
+        println!("cargo:rustc-link-lib=static=icudt");
+        println!("cargo:rustc-link-lib=static=icuin");
+    } else {
+        println!("cargo:rustc-link-lib=static=icudata");
+        println!("cargo:rustc-link-lib=static=icui18n");
+    }
 
     cc::Build::new()
         .file("wrapper/wrapper.cpp")
